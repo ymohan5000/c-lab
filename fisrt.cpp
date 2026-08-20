dd3417a94519743a5c57ab4fcf9a29456dd156b6
Mohan Yadav <ymohan5000@gmail.com>
2026-08-20 15:22:19 +0545

    hello

--- fisrt.cpp
+++ fisrt.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int a,b;
-    cout<<"enter two number ";
-cin>>a>>b;
-a%=b; //a+b;
-cout<<"the sum is "<<a<<endl;
+int main()
+{
+    int a,b,c;
+    cout<<"enter a three number";
+    cin>>a>>b>>c;
+    if(a>b&&a>c)
+    {
+        cout<<"a is longest";
+    }
+    if(b>a&&b>c)
+    {
+        cout<<"b is greatest";
+    }
+    cout<<"c is lognest";
 }

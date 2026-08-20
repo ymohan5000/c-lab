0a997e88b17f76c2c1b9a165ac2ed3f7a41a63f6
Mohan Yadav <ymohan5000@gmail.com>
2026-08-20 15:59:36 +0545

    m

---
+++ leep.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cout<<"enter a year";
+    cin>>year;
+    if(year/400)
+    cout<<"leep year";
+    else
+    cout<<"it is not leep year";
+}

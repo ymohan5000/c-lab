a231554d8a92c257a2364da75f1e54846115e884
Mohan Yadav <ymohan5000@gmail.com>
2026-08-20 15:26:40 +0545

    check

--- oddeven.cpp
+++ oddeven.cpp
@@ -1,1 +1,11 @@
-#
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter a num";
+    cin>>num;
+    if(num%2==0)
+        cout<<"even";
+    else
+    cout<<"odd";
+}

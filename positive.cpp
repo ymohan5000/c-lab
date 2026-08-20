9a2c9605f82eb81fbc92524f0e87c26d1420e5dc
Mohan Yadav <ymohan5000@gmail.com>
2026-08-20 15:30:24 +0545

    positive

---
+++ positive.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter a number";
+    cin>>num;
+    if(num>0)
+    cout<<"positive";
+    else 
+    cout<<"negative";
+}
